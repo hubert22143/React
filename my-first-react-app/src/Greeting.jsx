2ebfc89e409383a93d9e7c388797fa6182a0118f
hubert22143 <33151182+hubert22143@users.noreply.github.com>
2024-06-01 19:44:03 +0200

    My first react app
    Taking baby steps on React.

---
+++ my-first-react-app/src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h1>&quot; Some not important details &quot; </h1>;
+}
+
+export default Greeting;
---
+++ my-first-react-app/src/YummyFood.jsx
@@ -0,0 +1,5 @@
+import MyBestFood from "../src/kebsik.jpg";
+function BestFood(){
+    return <img src={MyBestFood} alt="Kebsik"></img>
+}
+export {BestFood as Kebsik};
