2775ac5f41957d90e74a2c4de93cc6b9537615e4
hubert22143 <33151182+hubert22143@users.noreply.github.com>
2024-06-24 20:59:01 +0200

    Learning rendering techniques

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
 import { Kebsik } from './YummyFood.jsx'
+import AnimalApp from './AnimalApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Greeting/>
-    <Kebsik/>
+    <AnimalApp/>
   </React.StrictMode>,
 )
